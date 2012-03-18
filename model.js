ffe22f553ef6017ae9767f573a9a9292890837b3
Abhishek Bhatnagar <abhishekbh@gmail.com>
2012-03-18 17:09:52 -0400

    Still need to implement new table for Tag <-> Datapoint relationship

--- model.js
+++ model.js
@@ -2,32 +2,35 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 function createModel() {
-  var Soc = new Schema({
+  var Soc = new Schema ({
       title: { type: String, required: true }
     , description: { type: String, required: false }
     , modified: { type: Date, default: Date.now }
   });
 
-  var DataPoint = new Schema({
+  var DataPoint = new Schema ({
       title: { type: String, required: true }
     , description: { type: String, required: true }
-    , location: [Location]
     , tag: [Tag]
-    , soc: [Soc]
     , modified: { type: Date, default: Date.now }
+    // foreign keys
+    , soc_id: { type: String, required: true }
+    , location_id: { type: String, required: true }
   });
 
-  var Location = new Schema({
+  var Location = new Schema ({
       title: { type: String, required: true }
     , latitude: { type: String, required: true }
     , longitude: { type: String, required: true }
     , modified: { type: Date, default: Date.now }
   });
 
-  var Tag = new Schema({
+  var Tag = new Schema ({
       title: { type: String, required: true }
     , description: { type: String, required: true }
     , modified: { type: Date, default: Date.now }
+    // foreign keys
+    , soc_id: { type: String, required: true }
   });
 
   // model definitions
