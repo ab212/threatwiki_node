42a86da65b3f5db8158fabf0b6d16520c20d4a15
Abhishek Bhatnagar <abhishekbh@gmail.com>
2012-05-27 16:09:34 -0400

    refs #15: FIXES saves on creates

--- routes.js
+++ routes.js
@@ -13,9 +13,10 @@ exports.soc = function(req, res){
   var splitemail = req.session.auth.google.user.email.split("@");
   var domain = splitemail[1];
   if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    res.render('socList', { locals: {
+
+  res.render('socList', { locals: {
       title: 'SOC Manager'
-      , scripts: ['javascript/utils.js', 'javascript/soc_list.js']
+      , scripts: ['/javascript/utils.js', '/javascript/soc_list.js']
     }});
   } else {
       //force logout if user doesn't meet conditions to view the page
@@ -30,7 +31,7 @@ exports.soc.create = function(req, res){
   if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
     res.render('soc', { locals: {
       title: 'Create an SOC'
-      , scripts: ['javascript/soc.js']
+      , scripts: ['/javascript/soc.js']
     }});
   } else {
       //force logout if user doesn't meet conditions to view the page
@@ -45,7 +46,7 @@ var domain = splitemail[1];
   if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
     res.render('datapointList', { locals: {
       title: 'Datapoint Manager'
-      , scripts: ['javascript/utils.js', 'javascript/datapoint_list.js']
+      , scripts: ['/javascript/utils.js', '/javascript/datapoint_list.js']
     }});
   } else {
       //force logout if user doesn't meet conditions to view the page
@@ -60,7 +61,7 @@ var domain = splitemail[1];
   if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
     res.render('datapoint', { locals: {
       title: 'Create Datapoint'
-      ,  scripts: ['javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', 'javascript/jquery.auto-geocoder.js']
+      ,  scripts: ['/javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', '/javascript/jquery.auto-geocoder.js']
     }});
   } else {
       //force logout if user doesn't meet conditions to view the page
@@ -75,7 +76,7 @@ var domain = splitemail[1];
   if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
     res.render('tagList', { locals: {
       title: 'Tag Manager'
-      , scripts: ['javascript/utils.js', 'javascript/tag_list.js']
+      , scripts: ['/javascript/utils.js', '/javascript/tag_list.js']
     }});
   } else {
     //force logout if user doesn't meet conditions to view the page
@@ -90,7 +91,7 @@ var domain = splitemail[1];
   if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
     res.render('tag', { locals: {
       title: 'Create Tag'
-      , scripts: ['javascript/tag.js']
+      , scripts: ['/javascript/tag.js']
     }});
   } else {
     //force logout if user doesn't meet conditions to view the page
