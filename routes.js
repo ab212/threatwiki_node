63384495a0e59a64a9594eb5c26dee0d51588789
Jerome <gagnonje@gmail.com>
2012-05-29 20:07:43 -0400

    fix bug: if user wasn't logged anymore, there was no redirect

--- routes.js
+++ routes.js
@@ -12,17 +12,22 @@ exports.index = function(req, res){
 
 exports.soc = function(req, res){
   //only render the page if we are logged in the system
-  var splitemail = req.session.auth.google.user.email.split("@");
-  var domain = splitemail[1];
-  if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    jQuery.getJSON('http://localhost:3000/api/soc?callback=?', function(socs) {
-      console.log(socs);
-      res.render('socList', { locals: {
-        title: 'SOC Manager'
-        , scripts: ['/javascript/soc_list.js']
-        , socs: socs
-      }});
-    });
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      jQuery.getJSON('http://localhost:3000/api/soc?callback=?', function(socs) {
+        console.log(socs);
+        res.render('socList', { locals: {
+          title: 'SOC Manager'
+          , scripts: ['/javascript/soc_list.js']
+          , socs: socs
+        }});
+      });
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
       res.redirect('/logout');
@@ -31,34 +36,44 @@ exports.soc = function(req, res){
 
 exports.soc.create = function(req, res){
   //only render the page if we are logged in the system
-  var splitemail = req.session.auth.google.user.email.split("@");
-  var domain = splitemail[1];
-  if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    res.render('soc', { locals: {
-      title: 'Create an SOC'
-      , scripts: ['/javascript/soc.js']
-    }});
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      res.render('soc', { locals: {
+        title: 'Create an SOC'
+        , scripts: ['/javascript/soc.js']
+      }});
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
-      //force logout if user doesn't meet conditions to view the page
-      res.redirect('/logout');
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
   }
 };
 
 exports.soc.edit = function(req, res){
   //only render the page if we are logged in the system
-  var splitemail = req.session.auth.google.user.email.split("@");
-  var domain = splitemail[1];
-  if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    var obj_id = req.query["id"];
-    console.log('http://localhost:3000/api/soc/'+ obj_id +'?callback=?');
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      var obj_id = req.query["id"];
+      console.log('http://localhost:3000/api/soc/'+ obj_id +'?callback=?');
 
-    jQuery.getJSON('http://localhost:3000/api/soc/'+ obj_id +'?callback=?', function(soc) {
-      res.render('soc', { locals: {
-        title: 'Edit SOC'
-        , scripts: ['/javascript/soc.js']
-        , soc: soc
-      }});
-    });
+      jQuery.getJSON('http://localhost:3000/api/soc/'+ obj_id +'?callback=?', function(soc) {
+        res.render('soc', { locals: {
+          title: 'Edit SOC'
+          , scripts: ['/javascript/soc.js']
+          , soc: soc
+        }});
+      });
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
       res.redirect('/logout');
@@ -66,33 +81,43 @@ exports.soc.edit = function(req, res){
 };
 
 exports.datapoint = function(req, res){
-  var splitemail = req.session.auth.google.user.email.split("@");
-var domain = splitemail[1];
   //only render the page if we are logged in the system
-  if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    jQuery.getJSON('http://localhost:3000/api/datapoint?callback=?', function(datapoints) {
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      jQuery.getJSON('http://localhost:3000/api/datapoint?callback=?', function(datapoints) {
       console.log(datapoints);
       res.render('datapointList', { locals: {
         title: 'Datapoint Manager'
         , scripts: ['/javascript/datapoint_list.js']
         , datapoints: datapoints
-      }});
-    })
+        }});
+      })
+    } else {
+      //force logout if user doesn't meet conditions to view the page
+      res.redirect('/logout');         
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
-      res.redirect('/logout');
+      res.redirect('/logout');  
   }
 };
 
 exports.datapoint.create = function(req, res){
-  var splitemail = req.session.auth.google.user.email.split("@");
-var domain = splitemail[1];
   //only render the page if we are logged in the system
-  if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    res.render('datapoint', { locals: {
-      title: 'Create Datapoint'
-      ,  scripts: ['/javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', '/javascript/jquery.auto-geocoder.js']
-    }});
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      res.render('datapoint', { locals: {
+        title: 'Create Datapoint'
+        ,  scripts: ['/javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', '/javascript/jquery.auto-geocoder.js']
+      }});
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
       res.redirect('/logout');
@@ -101,19 +126,24 @@ var domain = splitemail[1];
 
 exports.datapoint.edit = function(req, res){
   //only render the page if we are logged in the system
-  var splitemail = req.session.auth.google.user.email.split("@");
-  var domain = splitemail[1];
-  if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    var obj_id = req.query["id"];
-    console.log('http://localhost:3000/api/datapoint/'+ obj_id +'?callback=?');
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      var obj_id = req.query["id"];
+      console.log('http://localhost:3000/api/datapoint/'+ obj_id +'?callback=?');
 
-    jQuery.getJSON('http://localhost:3000/api/datapoint/'+ obj_id +'?callback=?', function(datapoint) {
-      res.render('datapoint', { locals: {
-        title: 'Edit Datapoint'
-        , scripts: ['/javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', '/javascript/jquery.auto-geocoder.js']
-        , datapoint: datapoint
-      }});
-    });
+      jQuery.getJSON('http://localhost:3000/api/datapoint/'+ obj_id +'?callback=?', function(datapoint) {
+        res.render('datapoint', { locals: {
+          title: 'Edit Datapoint'
+          , scripts: ['/javascript/datapoint.js', 'http://maps.googleapis.com/maps/api/js?sensor=false&key=AIzaSyCdCNPG_4JmvjQjbXVyB_W6Ena7b7CIqns&sensor=false', '/javascript/jquery.auto-geocoder.js']
+          , datapoint: datapoint
+        }});
+      });
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
       res.redirect('/logout');
@@ -121,18 +151,23 @@ exports.datapoint.edit = function(req, res){
 };
 
 exports.tag = function(req, res){
-  var splitemail = req.session.auth.google.user.email.split("@");
-var domain = splitemail[1];
   //only render the page if we are logged in the system
-  if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    jQuery.getJSON('http://localhost:3000/api/tag?callback=?', function(tags) {
-      console.log(tags);
-      res.render('tagList', { locals: {
-        title: 'Tag Manager'
-        , scripts: ['/javascript/tag_list.js']
-        , tags: tags
-      }});
-    })
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      jQuery.getJSON('http://localhost:3000/api/tag?callback=?', function(tags) {
+        console.log(tags);
+        res.render('tagList', { locals: {
+          title: 'Tag Manager'
+          , scripts: ['/javascript/tag_list.js']
+          , tags: tags
+        }});
+      })
+    } else {
+      //force logout if user doesn't meet conditions to view the page
+      res.redirect('/logout');
+    }
   } else {
     //force logout if user doesn't meet conditions to view the page
     res.redirect('/logout');
@@ -140,14 +175,18 @@ var domain = splitemail[1];
 };
 
 exports.tag.create = function(req, res){
-  var splitemail = req.session.auth.google.user.email.split("@");
-var domain = splitemail[1];
-  //only render the page if we are logged in the system
-  if(req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    res.render('tag', { locals: {
-      title: 'Create Tag'
-      , scripts: ['/javascript/tag.js']
-    }});
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      res.render('tag', { locals: {
+        title: 'Create Tag'
+        , scripts: ['/javascript/tag.js']
+      }});
+    } else {
+      //force logout if user doesn't meet conditions to view the page
+      res.redirect('/logout');
+    }
   } else {
     //force logout if user doesn't meet conditions to view the page
     res.redirect('/logout');
@@ -155,20 +194,23 @@ var domain = splitemail[1];
 };
 
 exports.tag.edit = function(req, res){
-  //only render the page if we are logged in the system
-  var splitemail = req.session.auth.google.user.email.split("@");
-  var domain = splitemail[1];
-  if (req.session.auth && req.session.auth.loggedIn && domain=='thesentinelproject.org'){
-    var obj_id = req.query["id"];
-    console.log('http://localhost:3000/api/tag/'+ obj_id +'?callback=?');
-
-    jQuery.getJSON('http://localhost:3000/api/tag/'+ obj_id +'?callback=?', function(tag) {
-      res.render('tag', { locals: {
-        title: 'Edit Tag'
-        , scripts: ['/javascript/tag.js']
-        , tag: tag
-      }});
-    });
+  if(req.session.auth && req.session.auth.loggedIn){
+    var splitemail = req.session.auth.google.user.email.split("@");
+    var domain = splitemail[1];
+    if (domain=='thesentinelproject.org'){
+      var obj_id = req.query["id"];
+      console.log('http://localhost:3000/api/tag/'+ obj_id +'?callback=?');
+      jQuery.getJSON('http://localhost:3000/api/tag/'+ obj_id +'?callback=?', function(tag) {
+        res.render('tag', { locals: {
+          title: 'Edit Tag'
+          , scripts: ['/javascript/tag.js']
+          , tag: tag
+        }});
+      });
+    } else {
+        //force logout if user doesn't meet conditions to view the page
+        res.redirect('/logout');
+    }
   } else {
       //force logout if user doesn't meet conditions to view the page
       res.redirect('/logout');
