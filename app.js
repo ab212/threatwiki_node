52ab88d83554817f8adbcf746b30d6f992ed540c
Jerome <gagnonje@gmail.com>
2012-12-20 09:59:30 -0500

    safe writing mode for mongoDB

--- app.js
+++ app.js
@@ -13,13 +13,10 @@ var application_root = __dirname
   //, user_api = require("./api/user_api")
 ;
 var app = express();
-//express 2.x
-//var app = module.exports = express.createServer();
-//app.enable("jsonp callback");
-//everyauth.helpExpress(app);
 
 // database
-mongoose.connect('mongodb://localhost/namp', function(err) {
+var mongoOptions = { db: { safe: true }};
+mongoose.connect('mongodb://localhost/namp', mongoOptions, function(err) {
   if (err) throw err;
 });
 
