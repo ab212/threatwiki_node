c08efc02ca3c144fe19e22a571b9f9200c728b1a
Jerome <gagnonje@gmail.com>
2013-03-09 16:04:11 -0500

    remove archive javascript from socview since its no longer in the UI

--- public/javascript/soc_view.js
+++ public/javascript/soc_view.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-
-  // #.put()
-  //someone clicked Archive on the update form
-  //TODO: Add confirmation dialog
-  $("[id*=archive]").click(function(){
-    var obj_id = $(this).attr("id").split("_");
-
-    jQuery.ajax({
-      url: "/api/datapoint/"+obj_id[1]+"/archive",
-      data: "archive=true",
-      type: 'PUT'
-    }).done(function() {
-      $("#row_"+obj_id[1]).fadeOut('slow', function() {});
-    });
-    return false;
-  });
-
   $.extend( $.fn.dataTableExt.oStdClasses, {
       "sWrapper": "dataTables_wrapper form-inline"
   } );
