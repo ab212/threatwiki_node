f474eefe6b373be80107759eb7bedf856c075676
Jerome <gagnonje@gmail.com>
2013-05-20 13:13:16 -0400

    add support for nodetime

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: 'Threatwiki' // optional
+  });
+}
 var application_root = __dirname
   , routes = require("./routes")
   , model = require("./model")
